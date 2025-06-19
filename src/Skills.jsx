0312119f35bccd6c571fd43b445e275cf770e7fd
Eric Molefe <molefejrtumelo@gmail.com>
2025-06-19 08:02:17 +0200

    Skills section done

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -33,6 +33,40 @@ function Skills() {
                             </p>
                     </div>
                 </div>
+                
+                <div className={styles['skills-card']}>
+                    <div className={styles['section-subheader']}>
+                        <h3 className={styles['section-subtitle']}>Cybersecurity & Network Security</h3>
+                        <div className={styles.underline}></div>
+                    </div>
+                    <div className={styles['skills-list']}>
+                        <p className={styles['skills-item']}>
+                            <b>Security Analysis: </b>
+                                Threat Detection, 
+                                Malware Analysis
+                            </p>
+                        <p className={styles['skills-item']}>
+                            <b>Network Security: </b>
+                                Traffic Analysis With Wireshark,
+                                Network Monitoring, 
+                                System Hardening
+                            </p>
+                        <p className={styles['skills-item']}>
+                            <b>Platforms and Tools: </b>
+                                 TryHackMe,
+                                 Virtualization Tools, 
+                                 Nagios Monitoring Tool
+                            </p>
+                        <p className={styles['skills-item']}>
+                            <b>Core Competencies: </b>
+                                Active Directory, Log Analysis, Secure Coding Practices
+                            </p>
+                        {/* <p className={styles['skills-item']}>
+                            <b>Specialized Projects: </b>
+                                Windows, Linux (Arch, Kali, Ubuntu)
+                            </p> */}
+                    </div>
+                </div>
             </div>
         </section>
     )
