dae229f5ce01f76dd8922109eb67cb5484d52261
Eric Molefe <molefejrtumelo@gmail.com>
2025-06-19 21:07:59 +0200

    working on hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,32 +4,38 @@ import profilePic from './assets/pic1-cropped.jpg'
 function Hero() {
     return (
         <section className={styles['hero-section']}>
-            <div className={styles['img-wrapper']}>
-                <img className={styles['profile-img']} src={profilePic} alt="Profile Picture"/>
-            </div>
-            <div className={styles['profile']}>
-                <h1 className={styles['profile-title']}>Tumelo Molefe</h1>
-                <h4 className={styles['profile-subtitle']}>Software Engineer</h4>
-                <p className={styles['profile-message']}>Bridging the gap between technology and creativity. I craft 
-                    digital experiences with code and bring imagination to life 
-                    through art.<br/>Welcome to my creative universe.</p>
-                
-                <ul className={styles['profile-socials']}>
-                    <li className={styles['social-link']}><a 
-                        href="https://github.com/EricMolefe"
-                        target="_blank">
-                        Github</a></li>
-                    <li className={styles['social-link']}><a 
-                        href="https://linkedin.com/in/tumelomolefe"
-                        target="_blank">
-                        Linkedin</a></li>
-                    <li className={styles['social-link']}><a href="https://www.youtube.com/@tmoCraft"
-                        target="_blank">
-                        Youtube</a></li>
-                    <li className={styles['social-link']}> <a href="https://github.com/EricMolefe"
-                        target="_blank">
-                        TryHackMe</a></li>
-                </ul>
+            <div className={styles['hero-container']}>
+                <div className={styles['img-wrapper']}>
+                    <img className={styles['profile-img']} src={profilePic} alt="Profile Picture"/>
+                </div>
+                <div className={styles['profile']}>
+                    <h1 className={styles['profile-title']}>TUMELO MOLEFE</h1>
+                    <h4 className={styles['profile-subtitle']}>Software Engineer</h4>
+                    <p className={styles['profile-message']}>Bridging the gap between technology and creativity. I craft 
+                        digital experiences with code and bring imagination to life 
+                        through art.<br/>Welcome to my creative universe.</p>
+                    
+                    <ul className={styles['profile-socials']}>
+                        <li className={styles['social-link']}><a 
+                            href="https://github.com/EricMolefe"
+                            target="_blank">
+                            Github</a></li>
+                        <li className={styles['social-link']}><a 
+                            href="https://linkedin.com/in/tumelomolefe"
+                            target="_blank">
+                            Linkedin</a></li>
+                        <li className={styles['social-link']}><a href="https://www.youtube.com/@tmoCraft"
+                            target="_blank">
+                            Youtube</a></li>
+                        <li className={styles['social-link']}> <a href="https://github.com/EricMolefe"
+                            target="_blank">
+                            TryHackMe</a></li>
+                    </ul>
+                    <div className={styles['profile-buttons']}>
+                        <div className={styles['cv-button']}>Download CV</div>
+                        <div className={styles['projects-button']}>See projects</div>
+                    </div>
+                </div>
             </div>
         </section>
     )
