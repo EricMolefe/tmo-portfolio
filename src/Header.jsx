cd605fd6d51e6d3a281153e8478f2ca587c90248
Eric Molefe <molefejrtumelo@gmail.com>
2025-06-19 19:34:09 +0200

    Test: responsiveness 3

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import HeaderStyles from './styles/Header.module.css'
+import styles from './styles/Header.module.css'
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false)
@@ -17,38 +17,40 @@ function Header() {
 
     return (
         <nav>
-            <h1 className={HeaderStyles.logo}>tmoCraft.</h1>
+            <div className={styles['nav-container']}>
+                <h1 className={styles.logo}>tmoCraft.</h1>
 
-            {/* Desktop Menu */}
-            <div className={HeaderStyles['desktop-menu']}>
-                <ul className={HeaderStyles['nav-links']}>
-                    {navLinks.map(link => (
-                        <li key={link.label} className={HeaderStyles['nav-item']}>
-                            <a href={link.href}>{link.label}</a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                {/* Desktop Menu */}
+                <div className={styles['desktop-menu']}>
+                    <ul className={styles['nav-links']}>
+                        {navLinks.map(link => (
+                            <li key={link.label} className={styles['nav-item']}>
+                                <a href={link.href}>{link.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-            {/* Hamburger Button */}
-            <div className={`${HeaderStyles['ham-menu']} ${isOpen ? HeaderStyles.active : ''}`}
-                onClick={toggleMenu}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+                {/* Hamburger Button */}
+                <div className={`${styles['ham-menu']} ${isOpen ? styles.active : ''}`}
+                    onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
 
-            {/* Mobile Menu */}
-            <div className={`${HeaderStyles['mobile-menu']} ${isOpen ? HeaderStyles.active : ''}`}>
-                <ul className={HeaderStyles['nav-links']}>
-                    {navLinks.map(link => (
-                        <li key={link.label} className={HeaderStyles['nav-item']}>
-                            <a href={link.href} onClick={closeMenu}>
-                                {link.label}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
+                {/* Mobile Menu */}
+                <div className={`${styles['mobile-menu']} ${isOpen ? styles.active : ''}`}>
+                    <ul className={styles['nav-links']}>
+                        {navLinks.map(link => (
+                            <li key={link.label} className={styles['nav-item']}>
+                                <a href={link.href} onClick={closeMenu}>
+                                    {link.label}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </nav>
     )
